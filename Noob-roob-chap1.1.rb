3118a5e8fb760b4ade05d158f65ad11ec7690fb5
Anthony Zotti <Theillbehaviored@gmail.com>
2014-05-17 23:00:02 -0400

    My first ruby files

---
+++ Noob-roob-chap1.1.rb
@@ -0,0 +1,4 @@
+puts "Whats your fav number?"
+number = gets.chomp()
+number = number.to_i + 1
+puts "I think the number " + number.to_s + " is a better number"
---
+++ Noob_roob_chap1.0.rb
@@ -0,0 +1,8 @@
+puts "Whats your first name, mate?"
+fname = gets.chomp()
+puts "...And, your middle name?"
+mname = gets.chomp()
+puts "You know whats last..."
+lname = gets.chomp()
+
+puts "Congratz " +fname + " " + mname + " " + lname + "!\nYou know how to enter basic text using a keyboard!"
