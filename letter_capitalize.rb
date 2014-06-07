26897003b7bd7b82a0070d9e102452899ebea6b9
Metis <metis@Metiss-MacBook-Air.local>
2014-06-07 10:06:15 -0400

    Implemented function as part of string class. Used gsub, regex, and str.capitalize

--- letter_capitalize.rb
+++ letter_capitalize.rb
@@ -1,35 +1,8 @@
-#Get input
-##Loop through
-#Capitalize every words
-#Return string of capitalized words
-
-class LetterCapitalize
-  def initialize
-    input
-    loop_words
-    display_output
-  end
-
-  def input
-    @new_word = ''
-    @words = gets.chomp.split(" ")
-  end
-
-
-  def loop_words
-    @words.each do |word|
-      @new_word +=  capitalize(word)\
-        + word[1,word.length] + ' '     
+class String
+  def titleize
+    self.gsub(/\w+/) do |word|
+      word.capitalize
     end
   end
-
-  def capitalize(word)
-    word[0] =  word[0].upcase
-  end
-
-  def display_output
-    puts @new_word
-  end
 end
-
-LetterCapitalize.new
+puts "lets see how this works".titleize
